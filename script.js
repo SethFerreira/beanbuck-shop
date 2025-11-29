9495902607e3db4f6020c2d7bc712b10680a00d8
Seth Ferreira <semfware@gmail.com>
2025-11-28 19:32:37 -0500

    feat: add toggle for footer social lists on mobile

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const toggleBtn = document.querySelector('.btn-close');
 const mainNav = document.querySelector('.main-nav');
+const socialListNav = document.querySelector('.social-list ul');
+const socialListNavIcon = document.querySelectorAll('.icon-text ion-icon');
 
 toggleBtn.addEventListener('click', () => {
     mainNav.classList.toggle('open');
+});
+
+socialListNavIcon.forEach(icon => {
+    icon.addEventListener('click', () => {
+        const ul = icon.closest('.social-list').querySelector('ul');
+        ul.classList.toggle('open'); 
+    });
 });
